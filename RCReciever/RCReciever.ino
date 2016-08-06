3c4c5efd288678ddbd54fe29c268a728a167de43
Alex Bowen <ajbowen249@gmail.com>
2016-08-06 16:44:02 -0400

    Basic throttle instruction transmission

--- RCReciever/RCReciever.ino
+++ RCReciever/RCReciever.ino
@@ -1,9 +1,57 @@
-void setup() {
-  // put your setup code here, to run once:
+//Wireless serial receiver relay.
+//Recieves serial data via software
+//serial, and retransmits over uart.
 
+#include <SoftwareSerial.h>
+#define BAUDRATE 9600
+#define PACKET_SIZE 3
+
+byte rxBuffer[PACKET_SIZE];
+
+void setup() {
+  Serial.begin(BAUDRATE);
+  while(!Serial){}
+  
+  Serial2.begin(BAUDRATE);
+  
+  Serial.println("Ready!");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if(Serial2.available()) {
+    char inChar = Serial2.read();
+    if(inChar != 0x55) return;
 
+    while(!Serial2.available()) {}
+    inChar = Serial2.read();
+    if(inChar != 0xAA) return;
+    
+    while(!Serial2.available()) {}
+    byte sum = Serial2.read();
+
+    while(Serial2.available() < PACKET_SIZE){}
+
+    for(int i = 0; i < PACKET_SIZE; i++){
+      rxBuffer[i] = Serial2.read();
+    }
+
+    if(CheckSum(rxBuffer, sum)){
+      Serial.print("instruction code: ");
+      Serial.print(rxBuffer[0]);
+      Serial.print(" arg 1: ");
+      Serial.print(rxBuffer[1]);
+      Serial.print(" arg 2: ");
+      Serial.println(rxBuffer[2]);
+    }
+  }
+}
+
+bool CheckSum(byte buffer[], byte sum) {
+  byte realSum = 0;
+
+  for(int i = 0; i < PACKET_SIZE; i++) {
+    realSum += buffer[i];
+  }
+
+  return realSum == sum;
 }
--- RCTransmitter/RCTransmitter.ino
+++ RCTransmitter/RCTransmitter.ino
@@ -1,7 +1,13 @@
 #define BAUDRATE 9600
 #define NULL_TX_INTERVAL 100000
-#define PACKET_SIZE 7
+#define PACKET_SIZE 3
 #define DIDDLE_SIZE 10
+
+#define THROTTLE_PIN 3
+#define THROTTLE_MIN 180
+#define THROTTLE_MAX 1000
+
+#define IX_SetMotors 0x53
 
 #include "Transmitter.h"
 #include "TimerOne.h"
@@ -11,19 +17,26 @@ void Write(byte* buffer, int size);
 Transmitter _transmitter(PACKET_SIZE, DIDDLE_SIZE, Write);
 
 void setup() {
-  Serial.begin(BAUDRATE);  
+  Serial.begin(BAUDRATE);
 }
 
 void loop() {
-  byte buffer[] = {'h', 'e', 'l', 'l', 'o', '\n', '\r'};
-  _transmitter.Transmit(buffer);
-  delay(100);
+  byte throttleBase = ThrottleBase();
+  byte instructionBuffer[3] = {IX_SetMotors, throttleBase, throttleBase};
 
-  byte buffer2[] = {'w', 'o', 'r', 'l', 'd', '\n', '\r'};
-  _transmitter.Transmit(buffer2);
+  _transmitter.Transmit(instructionBuffer);
+
   delay(100);
 }
 
 void Write(byte* buffer, int size) {
   Serial.write(buffer, size);
+}
+
+byte ThrottleBase(){
+  int throttleInput = analogRead(THROTTLE_PIN);
+
+  if(throttleInput < THROTTLE_MIN) return 0;
+  else if(throttleInput > THROTTLE_MAX) return 127;
+  else return (byte)(((double)((double)throttleInput - (double)THROTTLE_MIN)) / ((double)(THROTTLE_MAX) - (double)THROTTLE_MIN) * 127.0);
 }
